d2de6a0e62b9017fe7fa34d1111c6755dcd19799
Debjyoti Paul <2023mt93005@wilp.bits-pilani.ac.in>
2024-05-12 18:10:16 +0530

    added missed files

---
+++ front-end/src/components/workout-plans/WorkoutPlans.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const WorkoutPlans = () => (
+    <div>
+        <h1 className="text-2xl font-bold">Work out Plans</h1>
+        <p>Add or view Workout plans.</p>
+    </div>
+);
+
+export default WorkoutPlans;
