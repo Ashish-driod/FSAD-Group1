b3a64593614095490661ba76a23fa496ed053bb0
Debanjali MOndal <mondaldebanjali6@gmail.com>
2024-05-13 18:13:00 +0530

    Pushing the changes for Dashboard page design

--- front-end/src/components/dashboard/Dashboard.js
+++ front-end/src/components/dashboard/Dashboard.js
@@ -2,6 +2,19 @@ import React, { useState, useEffect } from 'react';
 import SidePanel from 'components/dashboard/sidepanel/SidePanel';
 import { Outlet } from 'react-router-dom';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import './Dashboard.css';
+
+const steps = 5000;
+const caloriesBurnt = 300;
+const heartRate = 80;
+
+//Testing code issue
+const data = {
+    steps: 5000,
+    caloriesBurnt: 300,
+    heartRate: 80
+};
+
 
 const Dashboard = () => {
     const [user, setUser] = useState(null);
@@ -36,6 +49,24 @@ const Dashboard = () => {
                     </div>
                 </header>
                 <Outlet /> {/* This is where the routed components will be displayed */}
+                <div className="dashboard-container">
+                    Good Job! Every Step counts ! Let's have look at your today's effort :
+                    <br /><br />
+                    <div className="section-container">
+                        <div className="section">
+                            <h2>Steps</h2>
+                            <p>{steps}</p>
+                        </div>
+                        <div className="section">
+                            <h2>Calories Burnt</h2>
+                            <p>{caloriesBurnt}</p>
+                        </div>
+                        <div className="section">
+                            <h2>Heart Rate</h2>
+                            <p>{heartRate}</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
